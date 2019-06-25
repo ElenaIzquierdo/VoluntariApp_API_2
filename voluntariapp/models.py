f36fce28fe4e997aff5dc0f658bca84f817a2911
Elena <eizquierdograu@gmail.com>
2019-06-25 14:55:28 +0200

    models created

--- voluntariapp/models.py
+++ voluntariapp/models.py
@@ -32,3 +32,43 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+class EventAttendee(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='eventattendee_user',
+                                null=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attending = models.BooleanField(default=False)
+
+
+class ForumTheme(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='forumtheme_creator',
+                                null=True)
+    title = models.CharField(blank=True, max_length=255)
+    created_date = models.DateField(default=timezone.now)
+    finished = models.BooleanField(blank=True)
+    description = models.TextField(blank=True, null=True)
+    tasks = models.TextField(blank=True, null=True)
+
+    def publish(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    content = models.CharField(max_length=400)
+    forumtheme = models.ForeignKey(ForumTheme, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+class Rate(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    rate = models.IntegerField(blank=True,null=True)
+    description = models.TextField(blank=True, null=True)
+
+class CentreInteres(models.Model):
+    objectius = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    state = models.TextField(blank=True, null=True)
+
